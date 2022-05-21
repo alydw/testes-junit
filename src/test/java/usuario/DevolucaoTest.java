b1737937e370af4a550bb965dbcc2e4e4cc18c47
Wdyla Epaminondas Carvalho <wdyla.carvalho@avanade.com>
2022-05-21 08:13:47 -0300

    ajustes

--- src/test/java/usuario/DevolucaoTest.java
+++ src/test/java/usuario/DevolucaoTest.java
@@ -1,31 +1,35 @@
 package usuario;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DevolucaoTest {
 
+    Devolucao devolucao;
+    @Before
+    public void setConfig(){
+        this.devolucao = new Devolucao();
+    }
+
     @Test
     public void devolucaoNaDataCorreta(){
-        Devolucao devolucao = new Devolucao();
-        devolucao.diasParaDevolucao(15);
-        devolucao.diasEntregues(12);
+        this.devolucao.diasParaDevolucao(15);
+        this.devolucao.diasEntregues(12);
         Assert.assertTrue(devolucao.getDataEfetuada());
     }
 
     @Test
     public void devolucaoAposAData(){
-        Devolucao devolucao = new Devolucao();
-        devolucao.diasParaDevolucao(20);
+        this.devolucao.diasParaDevolucao(20);
         devolucao.diasEntregues(22);
         Assert.assertFalse(devolucao.getDataEfetuada());
     }
 
     @Test
     public void dataDevolucaoRenovada(){
-        Devolucao devolucao = new Devolucao();
-        devolucao.diasParaDevolucao(3);
-        devolucao.diasAdicionados(10);
+        this.devolucao.diasParaDevolucao(3);
+        this.devolucao.diasAdicionados(10);
         Assert.assertEquals(13,devolucao.getDevolucaoRenovada(), 0.0);
     }
 }
--- src/test/java/usuario/ReservaTest.java
+++ src/test/java/usuario/ReservaTest.java
@@ -1,29 +1,33 @@
 package usuario;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ReservaTest {
 
+    Reserva reserva;
+    @Before
+    public void setConfig(){
+        this.reserva = new Reserva();
+    }
+
     @Test
     public void reservaSeExemplarDisponivel(){
-        Reserva reserva = new Reserva();
-        reserva.insereLivro(2);
+        this.reserva.insereLivro(2);
         Assert.assertTrue(reserva.realizaReserva());
     }
 
     @Test
     public void reservaNegadaPorExemplarIndisponivel(){
-        Reserva reserva = new Reserva();
-        reserva.insereLivro(0);
+        this.reserva.insereLivro(0);
         Assert.assertFalse(reserva.reservaIndisponivel());
     }
 
     @Test
     public void reservaNegadaPorExemplarEmEsprestimo(){
-        Reserva reserva = new Reserva();
-        reserva.insereLivro(3);
+        this.reserva.insereLivro(3);
         reserva.retiraLivro(3);
-        Assert.assertEquals("Reserva não realizada, pois livro já se encontra em uso", 0,reserva.getLivroIndisponivel(), 0);
+        Assert.assertEquals( 0,reserva.getLivroIndisponivel(), 0);
     }
 }
